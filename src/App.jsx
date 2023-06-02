8b58cec0018da1ce46034a8891ff91828713ece0
Ngaiti <henfanar@gmail.com>
2023-06-02 09:17:46 +0800

    Basic CSS done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-function MenuItem({ name, price }) {
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+function MenuItem({ name, price, nutritionInfo }) {
   return (
     <>
       <p>
         <strong>{name}</strong> | {price}
       </p>
+      <NutritionalInfo nutritionInfo={nutritionInfo} />
     </>
   )
 }
 
 function App() {
   return (
-    <>
-      <h1>Uncle Haris Fried Chicken</h1>
-      <Category title={"Fried Chicken"} foods={friedChicken} />
-      <Category title={"Dessert"} foods={dessert} />
-    </>
+    <div className='container my-5 text-center'>
+      <div className='col-md-4 p-3 bg-dark text-white border border-warning rounded'>
+        <h1>:o</h1>
+        <Category title={"Fried Chicken"} foods={friedChicken} />
+        <Category title={"Dessert"} foods={dessert} />
+      </div>
+    </div >
   )
 }
 
@@ -29,15 +35,42 @@ function Category({ title, foods }) {
 
 const friedChicken = (
   <>
-    <MenuItem name={"2pc Fried Chicken Set"} price={"RM 10.00"} />
-    <MenuItem name={"6 piece nugget"} price={"RM 6.00"} />
+    <MenuItem
+      name={"2pc Fried Chicken Set"}
+      price={"RM 10.00"}
+      nutritionInfo={{ calories: 540, protein: 31, carbs: 35, fat: 29 }}
+    />
+    <MenuItem
+      name={"6 piece nugget"}
+      price={"RM 6.00"}
+      nutritionInfo={{ calories: 270, protein: 13, carbs: 16, fat: 16 }}
+    />
   </>
 )
 
 const dessert = (
   <>
-    <MenuItem name={"Chocolate Sundae"} price={"RM 5.00"} />
-    <MenuItem name={"Apple Pie"} price={"RM 3.00"} />
+    <MenuItem
+      name={"Ice Cream"}
+      price={"RM 5.00"}
+      nutritionInfo={{ calories: 207, protein: 2, carbs: 33, fat: 6 }}
+    />
+    <MenuItem
+      name={"Apple Pie"}
+      price={"RM 2.00"}
+      nutritionInfo={{ calories: 270, protein: 1, carbs: 43, fat: 10 }}
+    />
   </>
 )
+
+function NutritionalInfo({ nutritionInfo }) {
+  return (
+    <div>
+      <p>Calories: {nutritionInfo.calories} kcal</p>
+      <p>Protein: {nutritionInfo.protein} g</p>
+      <p>Carbs: {nutritionInfo.carbs} g</p>
+      <p>Fat: {nutritionInfo.fat} g</p>
+    </div>
+  )
+}
 export default App;
